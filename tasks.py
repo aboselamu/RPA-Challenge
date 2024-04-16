fef7ecd76d23a7922b6f0ecbda77d2b2b8197fc9
aboselamu <34035713+aboselamu@users.noreply.github.com>
2024-04-16 03:48:20 +0200

    lk

--- tasks.py
+++ tasks.py
@@ -62,7 +62,7 @@ def main():
     search_list_selector = browser.find_element("xpath://*[@id='main-content-area']/div[2]/div[2]")
     
   
-    articles = search_list_selector.find_elements("xpath:.//article")
+    articles = search_list_selector.find_elements("xpath://article")
     titles_xpath = "//*[@id='main-content-area']/div[2]/div[2]/article/div[2]/div[1]/h3"
     titles = browser.find_elements(titles_xpath)
     
