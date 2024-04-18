148e5a3a840885122279b3e814f4b5aea1ba5d01
aboselamu <34035713+aboselamu@users.noreply.github.com>
2024-04-18 07:36:12 +0200

    The final

--- tasks.py
+++ tasks.py
@@ -1,22 +1,22 @@
-# import html
+import re
 import time
 import requests
 import logging
 from pathlib import Path
+from robocorp import vault
+from robocorp import excel
 from robocorp import storage
-from datetime import datetime, timedelta
 from datetime import datetime
 from robocorp.tasks import task
-# from RPA.Robocorp.WorkItems import WorkItems
-from robocorp import vault
+from datetime import datetime, timedelta
 from robocorp.tasks import get_output_dir
-# from RPA.Excel.Files import Files as Excel
-from robocorp import excel
 from RPA.Browser.Selenium import Selenium 
+
 logger = logging.getLogger(__name__)
-import re
 
 
+#@task
+#oppening the site aljazeera.com
 def opening_the_news_Site():
     logger.info("Opening the news site.")
     browser = Selenium()
@@ -29,30 +29,40 @@ def opening_the_news_Site():
     secrets =vault.get_secret('alijazeersite') 
 
     # Open browser with specified options
-    browser.open_available_browser(secrets["url"], browser_selection="Chrome", options=options)
+    browser.open_available_browser(secrets["url"], 
+                                        browser_selection="Chrome", 
+                                        options=options)
     return browser
 
-
+#searching for the phrase of the news
+# @task
 def search_the_phrase(browser, phrase):
+
     logger.info(f"Searching the phrase: {phrase}")
+    # if the site contains collecting cookies 
     try:
         browser.click_button('Allow all')
 
     except:
         pass
-        # finding the serach icon and field
+    
+    # finding the serach icon and field
     locator1 = "//button[@aria-pressed='false']//*[name()='svg']"
     browser.wait_until_page_contains_element(locator1, timeout=10)
     browser.click_element(locator1)
+    # inserting the search phrase in the input field
     browser.input_text("//input[@placeholder='Search']",phrase)
     browser.click_button("//button[@aria-label='Search Al Jazeera']")
+
+    # Trying to find it there is a realated articles with the search phrase
     try:
         locator2 = "//select[@id='search-sort-option']"
         browser.wait_until_element_is_visible(locator2, timeout=10)
         browser.click_element(locator2)
     except:
-        print("No news associated with the search phrase")
-        return
+        return "No news associated with the search phrase"
+
+    # sort by time
     dropdown_locator = "//select[@id='search-sort-option']/option[1]" 
     browser.click_element(dropdown_locator)
 
@@ -67,46 +77,53 @@ def retrive_data(browser, num_months_ago, search_phrase):
     # Handling the possible inputs
     if num_months_ago == 0:
         num_months_ago =1
+    # To compare the date
     current_date = datetime.now()
-    target_date = current_date - timedelta(days=num_months_ago * 30)  # Assuming each month has 30 days
+    target_date = current_date - timedelta(days=num_months_ago * 30)  # Assuming each month has 30 days)
 
-
-    # print(len(search_list_selector))
-    is_there_ShowMore = True
-
+    # to store articles for extraction
     articles_titiles = []
+
     browser.wait_until_element_is_visible("xpath://*[@id='main-content-area']/div[2]/div[2]", timeout=10)
+    
+    # to handle paggination
+    is_there_ShowMore = True
+    
     while is_there_ShowMore:
         
         # Search result section
-        
         search_list_selector = browser.find_element("xpath=//*[@id='main-content-area']/div[2]/div[2]")
         articles = browser.find_elements("tag:article", parent=search_list_selector)
+        # the show more button
         button_locator = browser.find_elements("tag:button", parent=search_list_selector)
         
+        # for each articles 
         for article in articles:
+            # getting excert section
             excert = browser.find_element("tag:p",parent=article)
+
+            # getting time and description of the post from excert
             time_of_post, description  = extract_before_ellipsis(excert.text)
             article_date = formated_article_date(time_of_post)
+
+            # check if the artices does contains date
             if(article_date == None):
                 continue
             try:
-                # print(article_date, target_date,)
-                if is_within_time_frame(article_date, target_date):
 
-                    # Now 'article' is a single WebElement, which can be used as a parent
+                # checking the article date is in the time period of the input
+                if is_within_time_frame(article_date, target_date):
 
                     title= browser.find_element("tag:h3", parent=article)
                     if title.text not in articles_titiles:
-                    
                         articles_titiles.append(title.text)
                         
                         # does the title or description contains money
                         # checking how many times the search keyword apears in title and description
                         no_of_search_phrase, contains = no_of_topic_and_money_amount(title.text, 
-                                                                            description, search_phrase)
-
-                        
+                                                                                      description, 
+                                                                                      search_phrase)
+                        # finding the imgae of each article
                         image = browser.find_element(locator="tag:img", parent=article)
                         image_url = image.get_attribute('src')
 
@@ -118,52 +135,47 @@ def retrive_data(browser, num_months_ago, search_phrase):
                         #update counter
                         counter+=1
 
-                        print("Title now")
-                        print(image_url)  # Or perform further actions with the image URL.
-                        print(picture_name,"The name of the picuture")
-                        print(output_path, "The output path of the picture")
-                        # print(image.scr)
-                        print(title.text) # This will print the text of the title within each article
-                        print( time_of_post, article_date)
-
-                        print(description)
-                        print("ONe article ends here")
             except Exception as e:
-                print(e, "Error now now")
-
-        # time.sleep(2)
+                print(e)
+        # try to locate and close the ads section
         try:
             ads_locator = browser.find_element("xpath=//button[@aria-label='Close Ad']")
             browser.click_button(ads_locator) 
+
         except Exception as e:
-            print(e,"My massege> NO Adds Found")
             pass
+        
+        # Trying to find if there is more article
         try: 
-            # Scroll the element into view
+            # Scroll the element into view the show more button
             browser.scroll_element_into_view(button_locator)
             browser.wait_until_element_is_enabled(button_locator, timeout=10)
+
 
             browser.click_button(button_locator)
             time.sleep(5)
             print("Botton Clicked")
     
         except Exception as e: 
-            print(e, "Error")
             is_there_ShowMore = False
+            pass
+
     return data
 
-
+# saving to excel
+# @task
 def save_data_to_Excel(workbook, data, sheet_name):
+    
     worksheet = workbook.worksheet(sheet_name)
+
+    # if there is articles save it inside the worksheet
     try:
-        print("INside data save to excel")
         for i in range(len(data)):
             row_to_append = [data[i]]
             worksheet.append_rows_to_worksheet(row_to_append, header=False)
         # Save the workbook
         # worksheet.save_workbook()
     except Exception as e:
-        print(e)
         pass 
 @task
 def main():
@@ -193,41 +205,45 @@ def main():
     # Retrieve the text content from the asset
     content = storage.get_text("parameters")
 
-    # # Split the content into lines
-    # topics_and_months = content.splitlines()
-    
-    # # for topic_and_month in topics_and_months:
-    # print(len(topic_and_month), "The length of topics and months")
-    # Assuming each line is "search_phrase,number_of_months"
+    # splitting it to search phrase and number of months
     search_phrase, number_of_months = content.split(',') 
         
     # Convert number_of_months to an integer
     number_of_months = int(number_of_months.strip())
-    search_phrase = search_phrase.strip()
-    print(number_of_months, search_phrase, "zzzzzzzzzzzzz")
-    # Create an output work item with this data as the payload
-    # workitems.outputs.create(payload={"search_phrase": search_phrase, "number_of_months": number_of_months})
 
+    # Performing cleaning the phrase
+    search_phrase = search_phrase.strip()
     
+    # Create an output work item with this data as the payload
+    # workitems.outputs.create(payload={"search_phrase": search_phrase, 
+    #                                         "number_of_months": number_of_months})
+    
+    # Sending search phrase to the search method
     search_the_phrase(browser_instance, search_phrase)
+
+    # Processing data
     data_retrieved =  retrive_data(browser_instance, number_of_months, search_phrase)
 
+    # Saving Data
     save_data_to_Excel(workbook, data_retrieved, sheet_name)
     workbook.save(excel_file_path)
 
+    # Saving the workbook
     workbook.save(excel_file_path)
-    print("This is Selamu's output")
     
     # Close the browser
-    # browser.close_all_browsers()
+    browser.close_all_browsers()
 
 
-
+# getting the date and description from the excert of the article
 def extract_before_ellipsis(text):
+    
+    # checking if the text contains the excert
     if len(text) <=0:
         return 
+
     # Split the text at '...'
-    date_part = "No Date"
+    date_part = ""
     description_part = ""
     try:
         parts = text.split(" ...")
@@ -239,16 +255,30 @@ def extract_before_ellipsis(text):
     description_part.replace("Â","")
 
     return date_part, description_part
+
+# formating the article's date
 def formated_article_date(date_extracted):
+
     # cleaning the date part
     date_extracted = date_extracted.strip()
 
-    # possible hours, minutes and seconds
-    possible_hms = ["second", "seconds","min\xadutes","minute", "minutes", "hour","hours"]
+    # Defining possible hours, minutes and seconds 
+    possible_hms = ["second", "seconds","min\xadutes", 
+                        "minute", "minutes", "hour","hours"]
+
     possible_days = ["day", "days"]
-    possible_months_format_One =["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    possible_months_format_Two =["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+    possible_months_format_One =["January", "Feburary", "March", "April", 
+                                    "May", "June", "July", "August", "September", 
+                                    "October", "November", "December"]
+
+    possible_months_format_Two =["Jan", "Feb", "Mar", "Apr",
+                                    "May", "Jun", "Jul", "Aug", 
+                                    "Sep", "Oct", "Nov", "Dec"]
+   
     current_date = datetime.now()
+   
+    # Formatting the date to make it more easy to compare and returning the article times
     try:   
         if(date_extracted.split(" ")[1]) in possible_hms:
             date_object = current_date
@@ -260,7 +290,9 @@ def formated_article_date(date_extracted):
             # Calculate the target date by subtracting the number of days from the current date
             date_object = current_date - timedelta(days=num_days)
             formatted_date = date_object.strftime("%Y%m%d")
+
             return formatted_date
+
         elif date_extracted.split(" ")[0] in possible_months_format_One:
             # Convert the date string to a datetime object
             date_object = datetime.strptime(date_extracted, "%B %d, %Y")
@@ -269,24 +301,33 @@ def formated_article_date(date_extracted):
             formatted_date = date_object.strftime("%Y%m%d")
     
             return formatted_date
+
         elif date_extracted.split(" ")[0] in possible_months_format_Two:
             # Convert the date string to a datetime object
             date_object = datetime.strptime(date_extracted, "%b %d, %Y")
             formatted_date= date_object.strftime("%Y%m%d")
+
             return formatted_date
 
     except Exception as e:
             return e, None
+
+# comparing if the article time is with in the date of given period of time
 def is_within_time_frame(article_date, target_date):
+
     # Convert article date string to a datetime object
     try:
         article_datetime = datetime.strptime(article_date, "%Y%m%d")
     except Exception as e:
         return e, False
+    
     # Check if the article date is within the time frame (since the target date)
     return article_datetime  >= target_date
 
+# checking if the topics and description contains money 
+# and how many times the title and description contains the search phrase
 def no_of_topic_and_money_amount(title, description, search_phrase):
+
     # Trying to find the number of times the title and description contains
     countT = title.split(" ").count(search_phrase)
     countD = description.split(" ").count(search_phrase)
@@ -297,4 +338,6 @@ def no_of_topic_and_money_amount(title, description, search_phrase):
     # Find all matches in the text
     matchesT = re.findall(pattern, title)
     matchesD = re.findall(pattern, description)
+
+    # returning the number of times money appears and if there is search phrase in both
     return countT + countD,  bool(matchesT + matchesD)
